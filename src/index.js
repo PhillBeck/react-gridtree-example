71822ee9a704d1eaffe04f783f6c378c3d096e76
Phillip Beck <phillipcbeck@gmail.com>
2017-09-29 11:29:40 -0300

    extended dataset

--- src/index.js
+++ src/index.js
@@ -5,37 +5,51 @@ import TreeGrid from 'react-treegrid';
 import registerServiceWorker from './registerServiceWorker';
 
 class App extends React.Component {
-    render() {
-        return (
-            <TreeGrid
-                data={[
-                    {
-                        Product: 'test1',
-                        Price: 5.35,
-                        children: [
-                            {
-                                Product: 'childProduct1',
-                                Price: 6.52,
-                                children: [
-                                    {
-                                        Product: 'childProduct1.1',
-                                        Price: 5.20
-                                    }
-                                ]
-                            }
-                        ]
-                    }, {
-                        Product: 'test2',
-                        Price: 2.50
-                    }
-                ]}
-                columnWidth={{
-                    Product: "70%",
-                    Price: "30%"
-                }}
-            />
-        )
+
+  constructor(props) {
+    super(props)
+    this.makeData = this.makeData.bind(this)
+    this.data = []
+  }
+
+  componentWillMount() {
+    for (let i = 0; i < 50; i++) {
+      this.data = this.data.concat(this.makeData(i))
     }
+  }
+
+  makeData(label, maxLevel, level) {
+    maxLevel = maxLevel || 49
+    level = level || 0
+
+    var obj = {
+      Product: `product_${label}.${level}`,
+      Qty: Math.random()
+    }
+
+    if (level < maxLevel) {
+      obj.children = this.makeData(label, maxLevel, level + 1)
+    }
+
+    return [obj]
+  }
+
+
+  render() {
+    console.log(this.data)
+    return (
+      <div className="mainApp">
+        <h2>This example displays a tree that consists of 50 objects, each 50 levels deep </h2>
+        <TreeGrid
+          data={this.data}
+          columnWidth={{
+            Product: "70%",
+            Qty: "30%"
+          }}
+        />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
